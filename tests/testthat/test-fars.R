0da100015beb57a7f7e1f650dcdc1818b6301e03
thie1e <c.thiele@gmx-topmail.de>
2017-01-25 11:34:45 +0100

    Remove tests and vignette

--- tests/testthat/test-fars.R
+++
@@ -1,5 +0,0 @@
-test_that("filenames", {
-    expect_that(make_filename(2013), is_a("character"))
-    expect_that(make_filename(2013), is_identical_to(make_filename("2013")))
-    expect_that(make_filename(2013), equals("accident_2013.csv.bz2"))
-})
